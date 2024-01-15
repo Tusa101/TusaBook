34edf23cc23c53a2532035c8911e73ca25e49e6a
Tusa101 <amtuska@gmail.com>
2024-01-15 14:56:23 +0300

    RazorPages

---
+++ TusaWeb/Migrations/20240112204609_UpdateCategoryTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TusaWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 4, 4, "Cartoon" },
+                    { 5, 5, "Fantasy" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
