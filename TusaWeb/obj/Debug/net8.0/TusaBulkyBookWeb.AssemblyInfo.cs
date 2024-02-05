80d3d4b77396276ab93a9673fc1cad93088d4875
Tusa101 <amtuska@gmail.com>
2024-02-05 13:37:15 +0300

    Azure Production Deployment

--- TusaWeb/obj/Debug/net8.0/TusaBulkyBookWeb.AssemblyInfo.cs
+++ TusaWeb/obj/Debug/net8.0/TusaBulkyBookWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TusaBulkyBookWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+895c3c65e0a51369e5cb824648fe73de5333cc81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa07d0dc224a2c41477a16095199e11b1e3c908")]
 [assembly: System.Reflection.AssemblyProductAttribute("TusaBulkyBookWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TusaBulkyBookWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
